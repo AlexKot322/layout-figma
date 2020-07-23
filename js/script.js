e2d79a7eeb05922866e18ee3c85fb14dedd900a3
AlexKot322 <kotovskov.schura@yandex.ru>
2020-07-23 21:51:12 +0300

    изменил

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('.work__card').hover(function() {
+        $('.circle5').toggleClass('circle_in');
+    }, function() {
+        $('.circle5').toggleClass('circle_in');
+    })
+});
+$(function() {
+    $('.work__card-1').hover(function() {
+        $('.circle3').toggleClass('circle_in');
+    }, function() {
+        $('.circle3').toggleClass('circle_in');
+    })
+});
